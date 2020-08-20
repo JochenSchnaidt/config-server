0b37e0ea1e84352d7342941e0900a966156192ca
jschnaidt <jochen.schnaidt@prodyna.com>
2020-08-20 23:33:49 +0200

    initial commit

---
+++ src/main/java/me/schnaidt/showcase/Application.java
@@ -0,0 +1,16 @@
+package me.schnaidt.showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] arguments) {
+    SpringApplication.run(Application.class, arguments);
+  }
+}
